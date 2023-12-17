6c9395e48cbdf89cd7974849f486d10cc41ed26e
takumi0712 <87654637+takumi0712@users.noreply.github.com>
2023-12-17 17:58:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pyxel
 
 class app:
     def __init__(self):
-        pyxel.init(256,192)
+        pyxel.init(256,256)
 
         self.x = 0
         self.y = 0
@@ -11,8 +11,6 @@ class app:
         self.x += 1
         self.y += 1
     def draw(self):
-        pyxel.cls(0)
-        pyxel.rect(self.x,self.y,10,10,2)
-
-if __name__ == "__main__":
-    app()
+        pyxel.cls(1)
+        pyxel.rect(self.x,self.y,100,100,2)
+app()
